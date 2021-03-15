2250d11512a51bd5b674714fff2a0e4b35da5ba4
Vito_Zhu@epam.com <Vito_Zhu@epam.com>
2021-03-15 19:11:07 +0800

    feat(*): 加入一些代码

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
-process.stdin.setEncoding('utf8');
+process.stdin.setEncoding('utf8')
 
 // This function reads only one line on console synchronously. After pressing `enter` key the console will stop listening for data.
 function readlineSync() {
     return new Promise((resolve, reject) => {
-        process.stdin.resume();
+        process.stdin.resume()
         process.stdin.on('data', function (data) {
-            process.stdin.pause(); // stops after one line reads
-            resolve(data);
-        });
-    });
+            process.stdin.pause() // stops after one line reads
+            resolve(data)
+        })
+    })
 }
 
 // entry point
 async function main() {
-    let inputLine1 = await readlineSync();
-    console.log('inputLine1 = ', inputLine1);
-    let inputLine2 = await readlineSync();
-    console.log('inputLine2 = ', inputLine2);
-    console.log('bye');
+    while (1) {
+        let inputLine = await readlineSync()
+        let text = JSON.parse(JSON.stringify(inputLine))
+        // console.log('>>>>>>>?????', text, typeof text, text === 'exit') // text=='exit'永远是false？？？？
+        if (text == 404) {
+            break
+        } else {
+            console.log('结果是', inputLine.split('').reverse().join(''))
+            console.log('------------------------------------------------------')
+        }
+    }
+
+    // let inputLine2 = await readlineSync()
+    // console.log('inputLine2 = ', inputLine2)
+
+    // let inputLine3 = await readlineSync()
+    // console.log('inputLine2 = ', inputLine3)
+
+
+    // readlineSync().then(rs => {
+    //     console.log('success', rs)
+    // }).catch(e => {
+    //     console.error('ERROR', e)
+    // })
 }
 
-main();
+main()
+// for (let i = 0; i < 10; i++) {
+//     main()
+//     // process.stdin.resume()
+// }
