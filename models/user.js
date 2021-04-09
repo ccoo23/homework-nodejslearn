2993dc5619225e573cfd11e84b2f540fd44513f6
Vito_Zhu@epam.com <Vito_Zhu@epam.com>
2021-04-09 19:06:02 +0800

    feat(*): 添加创建用户

--- models/user.js
+++ models/user.js
@@ -2,14 +2,32 @@
 import KNEX from '../libs/knex.js'
 
 class User {
-  static getUserById() {
+  static getUserById(id) {
     return new Promise((resolve) => {
       const db = KNEX
+      console.log('IDDDDDDDDD', id)
       db
-        .raw(`select userid as id from user`)
+        .raw(`select * from user where user_id=${id}`)
         .then((rs) => {
           resolve(rs)
         })
+    })
+  }
+
+  static createUser(params) {
+    return new Promise((resolve, reject) => {
+      const db = KNEX
+      console.log('params', params)
+      db('user').insert({
+        user_id: params.userid,
+        user_name: params.name,
+        password: params.password,
+        age: params.age
+      }).then((rs) => {
+        resolve(rs)
+      }).catch((err) => {
+        reject(err)
+      })
     })
   }
 }
--- module3/app.js
+++ module3/app.js
@@ -1,20 +1,40 @@
 import express from 'express'
 import User from '../models/user.js'
+import { v4 as uuidv4 } from 'uuid'
+import bodyParser from 'body-parser'
 
 const app = new express()
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', function(req, res){
     res.send('This is express server 1'); //服务器响应请求
 })
 
-app.get('/get', async function(req, res){
-    const [rs] = await User.getUserById()
-    console.log('RSSSSSSSSSSS', rs)
-    let arr = []
-    rs.forEach(e => {
-        arr.push(e.id)
-    })
-    res.send('get user info' + arr)
+app.get('/user/getById', async function(req, res){
+    const { id } = req.query
+    const [rs] = await User.getUserById(id)
+    console.log(rs)
+    let resp = ''
+    if (!rs[0]) {
+        resp = {
+            code: 10001,
+            msg: '未查询到结果'
+        }
+    } else {
+        resp = rs[0]
+    }
+    res.send(resp)
+})
+
+app.post('/user/create', async function(req, res){
+    const { name, password, age } = req.body
+    console.log('RESSSSSSSSS', req.body)
+    const userid = uuidv4()
+    const rs = await User.createUser({ name, password, age, userid })
+    res.send(rs)
 })
 
 app.listen(3001,function(){   //监听3001端口
